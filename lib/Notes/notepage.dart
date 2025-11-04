703ac1c67491e5483445af738b2517c92ff423fd
FelienZ <rajabfadh@gmail.com>
2025-11-04 23:32:41 +0700

    try to implement statefulwidget: create array notes to store data notes then pass to carouselNotes

--- lib/Notes/notepage.dart
+++ lib/Notes/notepage.dart
@@ -1,8 +1,59 @@
 import 'package:flutter/material.dart';
 import 'package:kuchi_notes/Notes/carouselnotes.dart';
 
-class Notepage extends StatelessWidget {
+class Notepage extends StatefulWidget {
   const Notepage({super.key});
+  @override
+  State<Notepage> createState() => _NoteState();
+}
+
+class _NoteState extends State<Notepage> {
+  List<Map<String, String>> notes = [];
+
+  void _addNote(String title, String content) {
+    setState(() {
+      notes.add({"title": title, "content": content});
+    });
+  }
+
+  void _showNoteDialog() {
+    String title = '';
+    String content = '';
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Tambah Catatan'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                decoration: InputDecoration(labelText: 'Judul'),
+                onChanged: (value) => title = value,
+              ),
+              TextField(
+                decoration: InputDecoration(labelText: 'Masukan Isi catatan'),
+                onChanged: (value) => content = value,
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              child: Text('Batal'),
+              onPressed: () => Navigator.pop(context),
+            ),
+            ElevatedButton(
+              child: Text('Simpan'),
+              onPressed: () {
+                _addNote(title, content);
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,9 +70,17 @@ class Notepage extends StatelessWidget {
           ),
         ),
       ),
-      body: SingleChildScrollView(child: Column(children: [Carouselnotes()])),
+      body: SingleChildScrollView(
+        child: notes.isNotEmpty
+            ? Column(children: [Carouselnotes(notes: notes)])
+            : SizedBox(
+                height: 700,
+                width: double.infinity,
+                child: Center(child: Text('Catatan Sedang Kosong')),
+              ),
+      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: _showNoteDialog,
         child: const Icon(Icons.add),
       ),
     );
