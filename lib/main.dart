6aeb44502c74722b77e8b1dbb12541e1440324e0
FelienZ <rajabfadh@gmail.com>
2025-11-19 22:09:13 +0700

    update title for project

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Kuchi Notesapp',
+      title: 'DosenQ',
       initialRoute: '/',
       routes: {
         '/': (context) => SplashScreen(),
