d673919c387c48b2e3f37cf8c0c6b1da48891c65
FelienZ <rajabfadh@gmail.com>
2025-11-19 21:45:28 +0700

    merge before push update

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:kuchi_notes/About/aboutpage.dart';
-import 'package:kuchi_notes/Help/Helppage.dart';
 import 'package:kuchi_notes/Home/myhome.dart';
 import 'package:kuchi_notes/Location/locationpage.dart';
+import 'package:kuchi_notes/splashscreen/splashscreen.dart';
 
 void main() => runApp(const MainApp());
 
@@ -15,7 +15,8 @@ class MainApp extends StatelessWidget {
       title: 'Kuchi Notesapp',
       initialRoute: '/',
       routes: {
-        '/': (context) => Homepage(),
+        '/': (context) => SplashScreen(),
+        '/home': (context) => Homepage(),
         '/location': (context) => Locationpage(),
         '/about': (context) => Aboutpage(),
       },
---
+++ lib/splashscreen/splashscreen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:kuchi_notes/Home/myhome.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.of(context).pushReplacement(
+        PageRouteBuilder(
+          transitionDuration: const Duration(milliseconds: 800),
+          pageBuilder: (context, animation, secondaryAnimation) => Homepage(),
+          transitionsBuilder: (context, animation, secondaryAnimation, child) {
+            return FadeTransition(opacity: animation, child: child);
+          },
+        ),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Container(
+          width: 300,
+          height: 300,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            shape: BoxShape.circle,
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Image.asset('assets/image/DosenQ.png'),
+          ),
+        ),
+      ),
+    );
+  }
+}
